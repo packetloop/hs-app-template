64de5d96deb20c953b01396242d249191b9fc4a3
Alexey Raga <alexey.raga@gmail.com>
2017-04-29 18:44:23 +1000

    Fix local container creation

--- scripts/build-librdkafka.sh
+++ scripts/build-librdkafka.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+PRJ=$PWD
 SRC=`mktemp -d 2>/dev/null || mktemp -d -t 'src'`
 
 cd ${SRC}
 git clone --depth 1 https://github.com/edenhill/librdkafka librdkafka
 
 cd librdkafka
-./configure
+./configure --prefix $PRJ/docker/rdkafka
 cd src
 make
-sudo make install
+make install
+sudo cp -r $PRJ/docker/rdkafka/* /usr/local/
