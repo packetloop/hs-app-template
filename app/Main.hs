54e90914639f481e1b44ca2877bdc848c75fdd44
Alexey Raga <alexey.raga@gmail.com>
2017-04-29 18:14:06 +1000

    Wire up SchemaRegistry

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,15 @@ module Main where
 import Arbor.Datadog.Conduit
 import Arbor.Logger
 import Control.Lens
+import Control.Monad                        (void)
 import Control.Monad.IO.Class
 import Control.Monad.Trans.AWS
 import Data.Conduit
 import Data.Semigroup                       ((<>))
 import HaskellWorks.Data.Conduit.Combinator
-import Kafka.Avro                           (SchemaRegistry, decodeWithSchema, schemaRegistry)
+import Kafka.Avro                           (schemaRegistry)
 import Kafka.Conduit.Source
-import Network.AWS.S3.Types                 (Region (..))
 import System.IO                            (stdout)
-
-import qualified Data.Conduit.List as L
 
 import App
 import App.Kafka
@@ -24,34 +22,30 @@ main :: IO ()
 main = do
   opt <- parseOptions
   env <- mkEnv opt
-  _ <- runGeoMute opt env
-  putStrLn "Exiting."
+  void . runApplication env opt $ do
+    logInfo $ show opt
 
-runGeoMute :: HasEnv e => Options -> e -> IO AppState
-runGeoMute opt env = runApplication env opt $ do
-  logInfo $ show opt
+    logInfo "Instantiating Schema Registry"
+    sr <- schemaRegistry (opt ^. optSchemaRegistryAddress)
 
-  logInfo "Instantiating Schema Registry"
-  sr <- schemaRegistry (opt ^. optSchemaRegistryAddress)
+    logInfo "Creating Kafka Consumer"
+    consumer <- mkConsumer opt
 
-  logInfo "Creating Kafka Consumer"
-  consumer <- mkConsumer opt
+    logInfo "Instantiating StatsD client"
+    stats <- mkStatsClient (DogStatsSettings (opt ^. optStatsdHost) (opt ^. optStatsdPort))
+    stags <- liftIO $ statsTags opt
+    let statsSink = metricSink (MetricName "hw-app-template") (toTags stags) (SampleRate 1.0) stats
 
-  logInfo "Instantiating StatsD client"
-  stats <- mkStatsClient (DogStatsSettings (opt ^. optStatsdHost) (opt ^. optStatsdPort))
-  stags <- liftIO $ statsTags opt
-  let statsSink = metricSink (MetricName "hw-app-template") (toTags stags) (SampleRate 1.0) stats
+    logInfo "Running Kafka Consumer"
+    runConduit $
+      kafkaSourceNoClose consumer (Timeout (opt ^. optKafkaPollTimeout))
+      .| throwLeftSatisfy isFatal
+      .| skipNonFatalExcept [isPollTimeout]
+      .| handleStream sr statsSink
+      .| everyN 100
+      .| commitOffsetsSink consumer
 
-  logInfo "Running Kafka Consumer"
-  runConduit $
-    kafkaSourceNoClose consumer (Timeout (opt ^. optKafkaPollTimeout))
-    .| throwLeftSatisfy isFatal
-    .| skipNonFatalExcept [isPollTimeout]
-    .| handleStream statsSink
-    .| everyN 100
-    .| commitOffsetsSink consumer
-
-  logError "Premature exit, must not happen."
+    logError "Premature exit, must not happen."
 
 statsTags :: MonadIO m => Options -> m [StatsTag]
 statsTags opts = do
--- app/Service.hs
+++ app/Service.hs
@@ -3,27 +3,34 @@ where
 
 import Arbor.Datadog.Conduit
 import Arbor.Logger
+import Control.Arrow                        (left)
+import Control.Monad.IO.Class
 import Data.ByteString                      (ByteString)
+import Data.ByteString.Lazy                 (fromStrict)
 import Data.Conduit
 import Data.Semigroup                       ((<>))
 import HaskellWorks.Data.Conduit.Combinator
-import Kafka.Avro
+import Kafka.Avro                           (SchemaRegistry, decodeWithSchema)
 import Kafka.Conduit.Source
 
 import qualified Data.Conduit.List as L
 
 import App
-import App.Kafka
 
 -- | Handles the stream of incoming messages.
 -- Change the return type to anything.
 -- Emit values downstream because offsets are committed based on their present.
 handleStream :: MonadApp m
-             => Sink Metric m ()
+             => SchemaRegistry
+             -> Sink Metric m ()
              -> Conduit (Either KafkaError (ConsumerRecord (Maybe ByteString) (Maybe ByteString))) m ()
-handleStream stats =
+handleStream sr stats =
   projectRights             -- getting rid of errors
   .| L.map crValue          -- extracting only value from consumer record
   .| L.catMaybes            -- discard empty values
   .| tap (countStats (MetricName "messages.received") stats)
+--  .| L.mapM (decodeMessage sr)  -- decode avro message. Uncomment when needed.
   .| L.mapM (\x -> logInfo ("Received message: " <> show x))
+
+decodeMessage :: MonadIO m => SchemaRegistry -> ByteString -> m (Either AppError ByteString)
+decodeMessage sr bs = left DecodeErr <$> decodeWithSchema sr (fromStrict bs)
