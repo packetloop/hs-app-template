2d305624263802b6442df3319137717bdea9074b
Jian Wan <jwan@arbor.net>
2017-05-01 08:22:28 +1000

    MAYH-4856 spec test sample for template

---
+++ tests/App/OptionsSpec.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module App.OptionsSpec
+  ( spec
+  ) where
+
+import Arbor.Datadog.Conduit (StatsTag (..))
+
+import App.Options
+
+import Test.Hspec
+
+{-# ANN module ("HLint: ignore Redundant do"  :: String) #-}
+
+spec :: Spec
+spec = describe "App.OptionsSpec" $ do
+    it "should parse tags" $
+      string2Tags "club_name:indica,deploy_id:manual" `shouldBe` [StatsTag ("club_name", "indica"), StatsTag ("deploy_id", "manual")]
