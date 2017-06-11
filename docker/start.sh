fbf25fca2998b1a2c70dc0fd00e5ef014d26d5f8
Alexey Raga <alexey.raga@gmail.com>
2017-06-11 19:35:49 +1000

    Added kafka queue size

--- docker/start.sh
+++ docker/start.sh
@@ -11,13 +11,14 @@ export STATSD_HOST=${STATSD_HOST:-$GATEWAY_IP}
 
 set -x
 hs-app-template \
-  ${AWS_REGION+                       --region              "${AWS_REGION}"                       } \
-  ${KAFKA_BOOTSTRAP+                  --bootstrap-broker    "${KAFKA_BOOTSTRAP}"                  } \
-  ${KAFKA_GROUP_ID+                   --group-id            "${KAFKA_GROUP_ID}"                   } \
-  ${KAFKA_SCHEMA_REGISTRY+            --schema-registry     "${KAFKA_SCHEMA_REGISTRY}"            } \
-  ${KAFKA_POLL_TIMEOUT+               --poll-timeout        "${KAFKA_POLL_TIMEOUT}"               } \
-  ${COMMANDS_TOPIC_IN+                --commands-topic      "${COMMANDS_TOPIC_IN}"                } \
-  ${STATSD_HOST+                      --statsd-host         "${STATSD_HOST}"                      } \
-  ${STATSD_SAMPLE_RATE+               --statsd-sample-rate  "${STATSD_SAMPLE_RATE}"               } \
-  ${STATSD_TAGS+                      --statsd-tags         "${STATSD_TAGS}"                      } \
-  ${LOG_LEVEL+                        --log-level           "${LOG_LEVEL}"                        }
+  ${AWS_REGION+                       --region                            "${AWS_REGION}"                       } \
+  ${KAFKA_BOOTSTRAP+                  --bootstrap-broker                  "${KAFKA_BOOTSTRAP}"                  } \
+  ${KAFKA_GROUP_ID+                   --group-id                          "${KAFKA_GROUP_ID}"                   } \
+  ${KAFKA_SCHEMA_REGISTRY+            --schema-registry                   "${KAFKA_SCHEMA_REGISTRY}"            } \
+  ${KAFKA_POLL_TIMEOUT+               --poll-timeout                      "${KAFKA_POLL_TIMEOUT}"               } \
+  ${KAFKA_QUEUED_MAX_MESSAGES_KBYTES+ --kafka-queued-max-messages-kbytes  "${KAFKA_QUEUED_MAX_MESSAGES_KBYTES}" } \
+  ${COMMANDS_TOPIC_IN+                --commands-topic                    "${COMMANDS_TOPIC_IN}"                } \
+  ${STATSD_HOST+                      --statsd-host                       "${STATSD_HOST}"                      } \
+  ${STATSD_SAMPLE_RATE+               --statsd-sample-rate                "${STATSD_SAMPLE_RATE}"               } \
+  ${STATSD_TAGS+                      --statsd-tags                       "${STATSD_TAGS}"                      } \
+  ${LOG_LEVEL+                        --log-level                         "${LOG_LEVEL}"                        }
